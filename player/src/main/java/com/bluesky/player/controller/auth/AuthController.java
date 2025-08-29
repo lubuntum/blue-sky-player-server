3354e17360f0decf564e1b49b8dfa26f5bb17424
lubuntum <lubuntum@gmail.com>
2025-08-29 07:15:09 +0700

    Добавлен метод регистрации аккаунта

--- player/src/main/java/com/bluesky/player/controller/auth/AuthController.java
+++ player/src/main/java/com/bluesky/player/controller/auth/AuthController.java
@@ -1,7 +1,14 @@
 package com.bluesky.player.controller.auth;
 
 import com.bluesky.player.database.service.AccountService;
+import com.bluesky.player.dto.account.AccountResponse;
+import com.bluesky.player.dto.account.CreateAccountRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AccountService accountService;
 
-
+    @PostMapping("/register")
+    public ResponseEntity<AccountResponse> registerAccount(@Valid @RequestBody CreateAccountRequest createAccountRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.createAccount(createAccountRequest));
+    }
 }
