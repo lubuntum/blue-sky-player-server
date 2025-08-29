be5bfcace72ee43a5c88923ee515cb1636ef7ac9
lubuntum <lubuntum@gmail.com>
2025-08-30 06:24:09 +0700

    jwt утилити класс для аутендификации + тест

---
+++ player/src/main/java/com/bluesky/player/util/jwt/JwtUtil.java
@@ -0,0 +1,59 @@
+package com.bluesky.player.util.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+    @Value("${app.secret.key}")
+    private String secretKey;
+
+    public String extractAccountEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getSigningKey())
+                .compact();
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractAccountEmail(token);
+        return (email.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
---
+++ player/src/test/java/com/bluesky/player/JwtUtilTest.java
@@ -0,0 +1,67 @@
+package com.bluesky.player;
+
+import com.bluesky.player.util.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.crypto.SecretKey;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtUtilTest {
+    @InjectMocks
+    private JwtUtil jwtUtil;
+
+    private final String secretKey = "test-secret-key-1234567890-1234567890-1234567890";
+    private final String testEmail = "test@example.com";
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @BeforeEach
+    void setUp(){
+        ReflectionTestUtils.setField(jwtUtil, "secretKey", secretKey);
+    }
+    @Test
+    void generateToken_WithExtraClaims_ShouldGenerateValidToken() {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("id", 123);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn(testEmail);
+
+        String token = jwtUtil.generateToken(extraClaims, userDetails);
+
+        assertNotNull(token);
+        assertEquals(3, token.split("\\.").length);
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        assertEquals(testEmail, claims.getSubject());
+        assertEquals(123, claims.get("id"));
+        assertNotNull(claims.getIssuedAt());
+        assertNotNull(claims.getExpiration());
+        assertTrue(claims.getExpiration().after(new Date()));
+    }
+
+
+}
