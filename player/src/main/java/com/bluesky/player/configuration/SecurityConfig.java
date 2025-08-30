4f169e72676a2f92acd3d9e0596f1512d6b39a20
lubuntum <lubuntum@gmail.com>
2025-08-31 06:56:27 +0700

    Обновил работу с паролем + создана но не протестирована конфигурация для Security

---
+++ player/src/main/java/com/bluesky/player/configuration/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.bluesky.player.configuration;
+
+import com.bluesky.player.database.service.AccountService;
+import com.bluesky.player.filter.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- player/src/main/java/com/bluesky/player/database/service/AccountService.java
+++ player/src/main/java/com/bluesky/player/database/service/AccountService.java
@@ -9,28 +9,32 @@ import com.bluesky.player.exception.account.AccountNotFoundException;
 import com.bluesky.player.exception.account.EmailAlreadyExistsException;
 import com.bluesky.player.util.password.PasswordUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.nio.channels.AcceptPendingException;
 
 @Service
 @AllArgsConstructor
 public class AccountService {
     public final AccountRepository accountRepository;
-    public final PasswordUtil passwordUtil;
+    public final PasswordEncoder passwordEncoder;
     public final AccountMapper accountMapper;
 
     public AccountResponse createAccount(CreateAccountRequest createAccountRequest) {
         if (accountRepository.existsByEmail(createAccountRequest.email()))
             throw new EmailAlreadyExistsException("Email address already busy");
         Account account = accountMapper.toEntity(createAccountRequest);
-        account.setPassword(passwordUtil.hashPassword(account.getPassword()));
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountMapper.toResponse(accountRepository.save(account));
     }
-    public AccountResponse getAccountByEmail(String email) {
+    public AccountResponse getAccountResponseByEmail(String email) {
         Account account  = accountRepository.findByEmail(email)
                 .orElseThrow(() -> new AccountNotFoundException("Account not found for " + email));
         return accountMapper.toResponse(account);
+    }
+    public Account getAccountByEmail(String email) {
+        return accountRepository
+                .findByEmail(email)
+                .orElseThrow(()-> new AccountNotFoundException("Account not found for " + email));
     }
 
 }
